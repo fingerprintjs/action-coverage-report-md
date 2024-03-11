c09ed3990362ef0b870e4dffb5f8ce7643402649
Ilya Taratukhin <ilya.taratukhin@fingerprint.com>
2024-03-11 21:03:49 +0100

    chore: fix other problems with configs

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,10 @@
 module.exports = {
+  preset: 'ts-jest',
   clearMocks: true,
+  testEnvironment: 'node',
   moduleFileExtensions: ['js', 'ts'],
   testMatch: ['**/*.test.ts'],
+  testPathIgnorePatterns: ['/node_modules/', '/dist/'],
   transform: {
     '^.+\\.ts$': 'ts-jest'
   },
